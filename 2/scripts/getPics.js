4c07c7dad923121265e92e06ffce270d249db92b
qinguiz <qinguiz@hotmail.com>
2016-09-14 11:17:24 +0800

    scroll

--- 2/scripts/getPics.js
+++ 2/scripts/getPics.js
@@ -1,9 +1,16 @@
 function getPics(){
 	var oUl = document.querySelector("#ul1");
 	var aLi = document.querySelectorAll("li");
+	var iPage = 1;
+	var b = true;
+	getList();
 	// var iLen = aLi.length;
-	ajax("get","php/getPics.php","cpage=1",function(data){ //url以html文档为当前文件的路径,所以../php/getPics.php是错误的
+	function getList(){
+		ajax("get","php/getPics.php","cpage="+iPage+"",function(data){ //url以html文档为当前文件的路径,所以../php/getPics.php是错误的
 		var data = JSON.parse(data);
+		if (!data.length) {
+			return;
+		}
 		for(var i=0;i<data.length;i++){
 			var _index = getShortH();
 			var oDiv = document.createElement("div");
@@ -17,7 +24,10 @@ function getPics(){
 			oDiv.appendChild(oP);
 			aLi[_index].appendChild(oDiv);
 		}
+		b = true;
 	})
+	}
+
 function getShortH(){
 	var index = 0;
 	var iH = aLi[index].offsetHeight;
@@ -28,6 +38,28 @@ function getShortH(){
 		}
 	}
 	return index;
+}
+
+window.onscroll = function(){
+	var _index = getShortH();
+	var oLi = aLi[_index];
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	if (getTop(oLi)+oLi.offsetHeight<document.documentElement.clientHeight+scrollTop) {
+		if (b) {
+			b = false;
+			iPage++;
+			getList();
+		}
+	}
+}
+
+function getTop(obj){
+	var iTop = 0;
+	while(obj){
+		iTop += obj.offsetTop;
+		obj = obj.offsetParent; //obj.offsetParent == body
+	}
+	return iTop;
 }
 }
 addLoadEvent(getPics);
